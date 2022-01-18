1c51ab5722c03b55ca1036440b38ab008444c71f
Chris Mochinski <cmochinski@gmail.com>
2022-01-17 21:26:58 -0600

    midway tutorial

--- index.ts
+++ index.ts
@@ -3,8 +3,10 @@ import * as _ from "lodash";
 async function hello() {
     return 'world';
 }
-
+//------------------------------//
 //IMPLICITLY TYPED: DECLARED AS NUMBER ALLOWS TYPESCRIPT TO KNOW WHAT IT IS
+//------------------------------//
+
 //it is "automatically inferred"
 let lucky = 23;
 //implicitly typed - declared number allows it to be automatically inferred 
@@ -13,15 +15,22 @@ let lucky = 23;
 // can't do this - it's a number, not a string! NAUGHTY
 
 //but if you go:
-
 let unlucky: any = 13;
 
+//you can go:
 unlucky: 'pancakes';
 
 console.log('unlucky is now:', unlucky)
 
+//------------------------------//
+//EXPLICITLY TYPED: no variable type yet - don't know what we're gonna be!
+//------------------------------//
 
-const awesome = (incomingArray : string[] | number[] ) => {
+let party; //automatically 'any' type because not assigned anything
+party = 5; //fine
+party = 'pants'; //fine
+
+const awesome = (incomingArray : string[] | number[] ) => { 
 
     let array = [incomingArray]
 
