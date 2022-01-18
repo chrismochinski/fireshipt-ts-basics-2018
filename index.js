b4001e1014405cd38cda3e2a56e5957b7d6cf6fd
Chris Mochinski <cmochinski@gmail.com>
2022-01-17 22:49:00 -0600

    last commit - tutorial complete

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ exports.__esModule = true;
 function hello() {
     return __awaiter(this, void 0, void 0, function () {
         return __generator(this, function (_a) {
-            return [2 /*return*/, 'world'];
+            return [2 /*return*/, "world"];
         });
     });
 }
@@ -48,41 +48,64 @@ function hello() {
 //------------------------------//
 //it is "automatically inferred"
 var lucky = 23;
-//implicitly typed - declared number allows it to be automatically inferred 
-//////// lucky = '23'; 
+//implicitly typed - declared number allows it to be automatically inferred
+//////// lucky = '23';
 // can't do this - it's a number, not a string! NAUGHTY
 //but if you go:
 var unlucky = 13;
 //you can go:
-unlucky: 'pancakes';
-console.log('unlucky is now:', unlucky);
+unlucky: "pancakes";
+console.log("unlucky is now:", unlucky);
 //------------------------------//
 //EXPLICITLY TYPED: no variable type yet - don't know what we're gonna be!
 //------------------------------//
 var party; //automatically 'any' type because not assigned anything
 party = 5; //fine
-party = 'pants'; //fine
+party = "pants"; //fine
 //fine:
 var font = "bold";
 var lasagna = 23;
 var dude = {
-    first: 'jeff',
-    last: 'delaney'
+    first: "jeff",
+    last: "delaney"
 };
 var bro = {
-    first: 'chris',
-    last: 'mochinski',
+    first: "chris",
+    last: "mochinski",
     loud: true
 };
+//---------------------------------------------------------//
+//  FUNCTIONS - types for arguments AND for return value
+//---------------------------------------------------------//
+function pow(x, y) {
+    return Math.pow(x, y).toString();
+}
+pow(5, 2);
+var arr = [];
+arr.push(1);
+arr.push('23');
+arr.push(false);
 //------------------------------//
-//objects - enforce shape with interface
+//  Typescript "Generics" 
+// wanna use a "type" inside of 
+//    a class or function?
 //------------------------------//
+//term: "RXjS Observable"
+var Observable = /** @class */ (function () {
+    function Observable(value) {
+        this.value = value;
+    }
+    return Observable;
+}());
+var x;
+var y;
+var z = new Observable(23);
 var awesome = function (incomingArray) {
     var array = [incomingArray];
     for (var i = 0; i < incomingArray.length; i++) {
-        console.log('next up!:', incomingArray[i]);
+        console.log("next up!:", incomingArray[i]);
     }
 };
 awesome([1, 2, 5, 9, 3, 6]);
-console.log('and now a mixture!');
-awesome(['barf', 'pancakes', 'yes', 'no', 'true']);
+console.log("and now a mixture!");
+awesome(["barf", "pancakes", "yes", "no", "true"]);
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as _ from "lodash";
 
 async function hello() {
-    return 'world';
+  return "world";
 }
 //------------------------------//
 //IMPLICITLY TYPED: DECLARED AS NUMBER ALLOWS TYPESCRIPT TO KNOW WHAT IT IS
@@ -9,18 +9,18 @@ async function hello() {
 
 //it is "automatically inferred"
 let lucky = 23;
-//implicitly typed - declared number allows it to be automatically inferred 
+//implicitly typed - declared number allows it to be automatically inferred
 
-//////// lucky = '23'; 
+//////// lucky = '23';
 // can't do this - it's a number, not a string! NAUGHTY
 
 //but if you go:
 let unlucky: any = 13;
 
 //you can go:
-unlucky: 'pancakes';
+unlucky: "pancakes";
 
-console.log('unlucky is now:', unlucky)
+console.log("unlucky is now:", unlucky);
 
 //------------------------------//
 //EXPLICITLY TYPED: no variable type yet - don't know what we're gonna be!
@@ -28,66 +28,96 @@ console.log('unlucky is now:', unlucky)
 
 let party; //automatically 'any' type because not assigned anything
 party = 5; //fine
-party = 'pants'; //fine
+party = "pants"; //fine
 
 //------------------------------//
 //Creating own type from scratch
 //------------------------------//
 
 //pascal style first letter uppercase
-type Style = 'bold' | 'italic' | 23 ; //separated with pipe = "union type"
+type Style = "bold" | "italic" | 23; //separated with pipe = "union type"
 
 //fine:
 let font: Style = "bold";
 
 let lasagna: Style = 23;
 
-//nope: 
+//nope:
 //         font = "beeeeeeans";
 
-//------------------------------//
-//objects - enforce shape with interface
-//------------------------------//
+//---------------------------------------//
+// Objects - enforce shape with interface
+//---------------------------------------//
 
-interface Person { // enforce shape of objects and stuff with an interface
-    first: string;
-    last: string;
-    [key: string] : any; //key with type of string = val any - allows additional properties!
+interface Person {
+  // enforce shape of objects and stuff with an interface
+  first: string;
+  last: string;
+  [key: string]: any; //key with type of string = val any - allows additional properties!
 }
 
-const dude : Person = {
-    first: 'jeff',
-    last: 'delaney'
+const dude: Person = {
+  first: "jeff",
+  last: "delaney",
+};
+
+const bro: Person = {
+  first: "chris",
+  last: "mochinski",
+  loud: true,
+};
+
+//---------------------------------------------------------//
+//  FUNCTIONS - types for arguments AND for return value
+//---------------------------------------------------------//
+
+function pow(x: number, y: number): string {
+  return Math.pow(x, y).toString();
 }
 
-const bro : Person = {
-    first: 'chris',
-    last: 'mochinski',
-    loud: true
-}
-
+pow(5, 2);
 
 //------------------------------//
-//objects - enforce shape with interface
+//  ARRAYS and question mark syntax! optional stuff
 //------------------------------//
 
+//question mark syntax make things optional
+type MyList = [number?, string?, boolean?] 
 
+const arr : MyList = [];
 
+arr.push(1);
+arr.push('23');
+arr.push(false);
 
+//------------------------------//
+//  Typescript "Generics" 
+// wanna use a "type" inside of 
+//    a class or function?
+//------------------------------//
 
+//term: "RXjS Observable"
 
-const awesome = (incomingArray : string[] | number[] ) => { 
-
-    let array = [incomingArray]
-
-    for (let i = 0; i < incomingArray.length; i++) {
-        console.log('next up!:', incomingArray[i]);
-    }
+class Observable<T> { //represents a variable type we can pass in
+    constructor(public value: T) {}
 }
 
+let x: Observable<number>;
+let y: Observable<Person>;
+let z = new Observable(23);
+
+
+
+const awesome = (incomingArray: string[] | number[]) => {
+  let array = [incomingArray];
+
+  for (let i = 0; i < incomingArray.length; i++) {
+    console.log("next up!:", incomingArray[i]);
+  }
+};
 
 awesome([1, 2, 5, 9, 3, 6]);
 
-console.log('and now a mixture!');
+console.log("and now a mixture!");
 
-awesome(['barf', 'pancakes', 'yes', 'no', 'true']);
+awesome(["barf", "pancakes", "yes", "no", "true"]);
